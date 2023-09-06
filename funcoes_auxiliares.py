2790e0694bc319a38c941ac17ac479baec363e06
Sarah Barbosa <106551299+SarahBarbosa@users.noreply.github.com>
2023-09-06 17:11:11 -0300

    Adicionando funções S01

---
+++ funcoes_auxiliares.py
@@ -0,0 +1,82 @@
+import pandas as pd
+
+def descrever_colunas(dados):
+    """
+    Esta função recebe um DataFrame e exibe informações resumidas sobre suas 
+    colunas, incluindo o tipo de dados e, se houver mais de 4 valores únicos, 
+    a contagem de valores únicos.
+
+    Parâmetros:
+    - dados (pd.DataFrame): O DataFrame contendo os dados a serem inspecionados.
+    """
+    for col in dados.columns:
+        tipo_de_dados = dados[col].dtype
+        num_valores_unicos = dados[col].nunique()
+
+        if num_valores_unicos > 4:
+            print(f'Coluna {col}: {tipo_de_dados}, {num_valores_unicos}/{len(dados)} valores únicos')
+        else:
+            valores_unicos = ', '.join(map(str, dados[col].unique()))
+            print(f'Coluna {col}: {tipo_de_dados}, Valores Únicos: {valores_unicos}')
+
+
+def descrever_dados_faltantes(dados, coluna):
+    """
+    Esta função recebe um DataFrame e o nome de uma coluna e exibe informações 
+    sobre os dados faltantes nessa coluna.
+
+    Parâmetros:
+    - dados (pd.DataFrame): O DataFrame contendo os dados a serem inspecionados.
+    - coluna (str): O nome da coluna a ser analisada.
+    """
+    dados_faltantes = dados[coluna].isnull().sum()
+    porcentagem_faltantes = round((dados_faltantes / len(dados)) * 100, 2)
+
+    print(f'Dados faltantes na coluna {coluna}: {dados_faltantes}. \
+     Isso representa {porcentagem_faltantes}% do total.')
+
+def preprocessamento_binarizacao(df, colunas_sim_nao, colunas_multiclasses, colunas_mistas):
+    """
+    Realiza o pré-processamento de um DataFrame, convertendo colunas categóricas 
+    em formatos binários ou one-hot encoding.
+
+    Parâmetros:
+    - df (pd.DataFrame): O DataFrame que contém os dados a serem pré-processados.
+    - colunas_sim_nao (list): Uma lista de nomes de colunas que contêm valores 
+    'Yes' e 'No' a serem binarizados (0 ou 1).
+    - colunas_multiclasses (list): Uma lista de nomes de colunas com categorias 
+    múltiplas a serem transformadas em one-hot encoding.
+    - colunas_mistas (list): Uma lista de nomes de colunas que podem conter 
+    valores 'Yes', 'No', 'No internet service' e 'No phone service'.
+
+    Retorna:
+    - pd.DataFrame: O DataFrame pré-processado com as colunas convertidas.
+    """
+    # Binariza colunas com valores 'Yes' e 'No'
+    df[colunas_sim_nao] = df[colunas_sim_nao].replace({'Yes': 1, 'No': 0})
+    
+    # Converte a coluna 'customer.gender' em binário (Female: 1, Male: 0)
+    df['customer.gender'] = df['customer.gender'].replace({'Female': 1, 'Male': 0})
+
+    # Realiza one-hot encoding nas colunas de multiclasses
+    df = pd.get_dummies(df, columns=colunas_multiclasses, dtype=int)
+
+    # Função para criar colunas de serviço
+    def criando_coluna_sem_servico(row):
+        no_phone_service = 1 if 'No phone service' in row.values else 0
+        no_internet_service = 1 if 'No internet service' in row.values else 0
+        return pd.Series({'NoPhoneService': no_phone_service, 
+        'NoInternetService': no_internet_service})
+
+    # Aplica a função de criação de colunas de serviço
+    colunas_sem_servico = df[colunas_mistas].apply(criando_coluna_sem_servico, axis=1)
+
+    # Binariza colunas mistas
+    df[colunas_mistas] = df[colunas_mistas].replace({'Yes': 1, 'No': 0, 
+                                                     'No internet service': 0, 
+                                                     'No phone service': 0})
+
+    # Concatena as colunas de serviço ao DataFrame
+    return pd.concat([df, colunas_sem_servico], axis=1)
+
+
---
+++ funcoes_plot.py
@@ -0,0 +1,90 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_countplots(data, coluna_hue, grupos, paleta, figsize=(12, 8)):
+    """
+    Gera subplots de gráficos de barras com contagem para várias colunas 
+    categóricas.
+
+    Parâmetros:
+    - data (pd.DataFrame): O DataFrame contendo os dados a serem visualizados.
+    - coluna_hue (str): O nome da coluna que será usada para a diferenciação das 
+    barras por cores (hue).
+    - grupos (list): Uma lista de nomes de colunas categóricas a serem 
+    visualizadas.
+    - paleta (str ou list): Uma paleta de cores para a diferenciação das barras.
+    Pode ser uma string de paleta ou uma lista de cores.
+    - figsize (tuple): Tamanho da figura (largura, altura).
+    """
+    num_grupos = len(grupos)
+    cols = 2
+    linhas = int(np.ceil(num_grupos / cols))
+
+    fig, axes = plt.subplots(nrows=linhas, ncols=cols, figsize=figsize)
+    axes = axes.flatten()  # Transforma a matriz de eixos em uma matriz 1D
+
+    for i, grupo in enumerate(grupos):
+        if i < num_grupos:
+            eixo = axes[i]  # Acessa o eixo diretamente da matriz 1D
+            sns.countplot(x=grupo,
+                          data=data,
+                          hue=coluna_hue,
+                          palette=paleta,
+                          alpha=0.8,
+                          ax=eixo)
+            sns.despine(right=True, top=True, left = True)
+            eixo.set(ylabel=None)
+            eixo.tick_params(left=False)
+            eixo.set(yticklabels=[])
+            eixo.legend([], [], frameon=False)
+            eixo.set_xlabel(grupo.split('.')[-1])
+            eixo.set_ylabel('')
+
+            # Adiciona o valor da frequência em cima de cada barra
+            for p in eixo.patches:
+              x = p.get_x() + p.get_width() / 2
+              y = p.get_height()
+              eixo.annotate(f'{y}\n({y/len(data)*100:.2f}%)\n', (x, y),
+              ha='center', va='bottom', color='gray')
+
+    # Remove quaisquer subplots vazios adicionais
+    for i in range(num_grupos, linhas * cols):
+        fig.delaxes(axes[i])
+
+    # Cria uma legenda fora dos subplots
+    handles, labels = eixo.get_legend_handles_labels()
+    legenda = fig.legend(handles,
+                         labels,
+                         loc='upper center',
+                         bbox_to_anchor=(0.5, 1.13),
+                         ncol=2)
+    legenda.set_title('Churn')
+
+    plt.tight_layout()
+
+def adicionar_estatisticas(ax, dados, metrica, texto_y, cor):
+    """
+    Adiciona estatísticas resumidas a um subplot.
+
+    Parâmetros:
+    - ax (matplotlib.axes._subplots.AxesSubplot): O subplot ao qual as 
+    estatísticas serão adicionadas.
+    - dados (pd.DataFrame): O DataFrame contendo os dados a serem analisados.
+    - metrica (str): A métrica para a qual as estatísticas serão calculadas 
+    (por exemplo, 'total_charges').
+    - texto_y (float): A posição vertical do texto no subplot.
+    - cor (str): A cor do texto.
+    """
+    valores_estatisticos = dados.groupby('Churn')[metrica].agg(['mean', 'median', 'min', 'max'])
+
+    texto = f'Média: {valores_estatisticos["mean"][0]:.2f} | {valores_estatisticos["mean"][1]:.2f}\n'
+    texto += f'Mediana: {valores_estatisticos["median"][0]:.2f} | {valores_estatisticos["median"][1]:.2f}\n'
+    texto += f'Mínimo: {valores_estatisticos["min"][0]:.2f} | {valores_estatisticos["min"][1]:.2f}\n'
+    texto += f'Máximo: {valores_estatisticos["max"][0]:.2f} | {valores_estatisticos["max"][1]:.2f}'
+
+    ax.text(0.5, texto_y, texto, transform=ax.transAxes, fontsize=12, color=cor, ha='center')
