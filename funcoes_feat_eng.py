f632ba7f401aa47e5a250e53ce5fcfe42480bfc6
Sarah Barbosa <106551299+SarahBarbosa@users.noreply.github.com>
2023-09-08 17:52:14 -0300

    modificação para classe de transformação

--- funcoes_feat_eng.py
+++ funcoes_feat_eng.py
@@ -1,57 +1,84 @@
 import pandas as pd
 import numpy as np
+from sklearn.base import TransformerMixin, BaseEstimator
 
-def criar_segmentos(dados):
+class ProcessadorDadosCustomizado(BaseEstimator, TransformerMixin):
     """
-    Cria segmentos com base na duração do cliente e nas mensalidades.
-
-    Parâmetros:
-        - dados (DataFrame): DataFrame contendo os dados dos clientes.
-    """
-    # Segmentos anuais para Tenure
-    intervalos = [0, 12, 24, 36, 48, 60, 72]
-    rotulos = ['0-1 Ano', '1-2 Anos', '2-3 Anos', '3-4 Anos', '4-5 Anos', '5-6 Anos']
+    Classe para pré-processamento de dados personalizado.
     
-    dados['Tenure_anual'] = pd.cut(dados['Tenure'], bins = intervalos, labels = rotulos)
-
-    # Segmentos para clientes de acordo com os gastos mensais e total
-    rotulos = ['Baixo', 'Moderado', 'Alto', 'Muito Alto']
-    dados['Charges_monthly_categories'] = pd.cut(dados['Charges_monthly'], bins = 4, labels = rotulos)
-    dados['Charges_total_categories'] = pd.cut(dados['Charges_total'], bins = 4, labels = rotulos)
-
-def criar_indicador_internet_suporte(dados):
-    """
-    Cria um indicador de suporte com base em várias.
-
     Parâmetros:
-        - dados (DataFrame): DataFrame contendo os dados dos clientes.
+    adicionar_todas_modificacoes (bool): Indica se todas as modificações 
+    devem ser aplicadas ao conjunto de dados.
     """
-    dados['No_internet_support_service'] = np.where((dados['Onlinesecurity'] != 1) | 
-    (dados['Onlinebackup'] != 1) | (dados['Deviceprotection'] != 1), 1, 0)
 
-def criar_indicador_todos_servicos_internet(dados):
-    """
-    Cria um indicador para o número de serviços de internet que atendem a 
-    determinadas condições.
+    def __init__(self, adicionar_todas_modificacoes=True):
+        self.adicionar_todas_modificacoes = adicionar_todas_modificacoes
 
-    Parâmetros:
-        - dados (DataFrame): DataFrame contendo os dados dos clientes.
-    """
-    servicos = ['Onlinesecurity', 'Onlinebackup', 'Deviceprotection',
-                'Techsupport', 'Streamingtv', 'Streamingmovies']
+    def fit(self, X):
+        """
+        Método de treinamento do estimador.
+        
+        Parâmetros:
+        X (array-like): Os dados de entrada.
+        """
+        return self
 
-    for servico in servicos:
-        dados[f'{servico}_temp'] = (dados[servico] == 1) & (dados['Nophoneservice'] == 0) & (dados['Nointernetservice'] == 0)
+    def transform(self, X):
+        """
+        Método de transformação dos dados.
 
-    dados['Num_internet_services'] = dados[[f'{servico}_temp' for servico in servicos]].sum(axis=1)
-    dados.drop(columns=[f'{servico}_temp' for servico in servicos], inplace=True)
+        Parâmetros:
+        X (array-like): Os dados de entrada.
+        """
+        data = pd.DataFrame(X)
 
-def calcular_estatisticas_gastos(dados):
-    """
-    Calcula estatísticas adicionais dos clientes.
+        if self.adicionar_todas_modificacoes:
+            self.criar_segmentos(data)
+            self.criar_indicador_todos_servicos_internet(data)
+            self.calcular_estatisticas_despesas(data)
 
-    Parâmetros:
-        - dados (DataFrame): DataFrame contendo os dados dos clientes.
-    """
-    dados['Avg_charges_monthly'] = dados['Charges_total'] / (dados['Tenure'])
-    dados['Avg_charges_services'] = dados['Charges_total'] / (dados['Num_internet_services'] + 1)
+        return  data
+
+    def criar_segmentos(self, data):
+        """
+        Cria colunas de segmentos com base na coluna 'Tenure'.
+
+        Parâmetros:
+        data (DataFrame): O DataFrame de dados.
+        """
+        intervalos = [0, 12, 24, 36, 48, 60, 72]
+        rotulos = ['0-1 Year', '1-2 Year', '2-3 Year', '3-4 Year', '4-5 Year', '5-6 Year']
+        data['Tenure_category'] = pd.cut(data['Tenure'], bins=intervalos, 
+                                         labels=rotulos)
+
+        rotulos = ['Low', 'Moderate', 'High', 'Very High']
+        data['Charges_monthly_cat'] = pd.cut(data['Charges_monthly'], bins = 4, labels=rotulos)
+        data['Charges_total_cat'] = pd.cut(data['Charges_total'], bins = 4, labels=rotulos)
+
+    def criar_indicador_todos_servicos_internet(self, data):
+        """
+        Cria uma coluna indicadora para todos os serviços de internet.
+
+        Parâmetros:
+        data (DataFrame): O DataFrame de dados.
+
+        Retorna:
+        None
+        """
+        servicos = ['Onlinesecurity', 'Onlinebackup', 'Deviceprotection',
+                    'Techsupport', 'Streamingtv', 'Streamingmovies']
+
+        for servico in servicos:
+            data[f'{servico}_temp'] = (data[servico] == 1) & (data['Nointernetservice'] == 0)
+
+        data['Num_internet_service'] = data[[f'{servico}_temp' for servico in servicos]].sum(axis=1)
+        data.drop(columns=[f'{servico}_temp' for servico in servicos], inplace=True)
+
+    def calcular_estatisticas_despesas(self, data):
+        """
+        Calcula estatísticas de despesas e cria colunas correspondentes.
+
+        Parâmetros:
+        data (DataFrame): O DataFrame de dados.
+        """
+        data['charge_per_internet_service'] = data['Charges_monthly'] / (data['Num_internet_service'] + 1)
